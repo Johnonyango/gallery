bd6f4ec7b9e076c525824bf533dfea13b663225b
Johnonyango <j.yayah7@gmail.com>
2019-08-24 12:40:38 +0300

    implemented template inheritance

--- photos/views.py
+++ photos/views.py
@@ -6,14 +6,30 @@ import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to gallery section')
+    return render(request, 'welcome.html')
 
 def photos_of_day(request):
     date = dt.date.today()
+    date = dt.date.today()
+    return render(request, 'all-news/today_photos.html', {"date": date,})
+
+
+    
+# View Function to present news from past days
+def past_days_photos(request,past_date):
+    
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+
+    day = convert_dates(date)
     html = f'''
         <html>
             <body>
-                <h1> {date.day}-{date.month}-{date.year}</h1>
+                <h1>Photos taken on {day} {date.day}-{date.month}-{date.year}</h1>
             </body>
         </html>
             '''
