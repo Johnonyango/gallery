2da1a4591b34a7720acb8740e9ceb22f3118c0a0
Johnonyango <j.yayah7@gmail.com>
2019-08-25 09:05:03 +0300

    create model for tags

--- gallery/settings.py
+++ gallery/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'gallery',
         'USER': 'moringa',
-    'PASSWORD':'12345',
+        'PASSWORD':'1234',
     }
 }
 
